c9f41b4142835eb325159ed1bd319019624dbc52
Oisin <oisin.leonard@gmail.com>
2026-01-05 10:58:16 +0000

    Revised utitity functions using copilot

--- generator/utilities/align_country_codes.py
+++ generator/utilities/align_country_codes.py
@@ -6,21 +6,21 @@ from beartype import beartype
 @beartype
 def align_country_codes(
     series:pd.Series,
-    proba_comm_ip:float=0.95,
-    proba_comm_card:float=0.99
+    proba_comm_ip:float=0.05,
+    proba_comm_card:float=0.01
     ) -> pd.Series:
     """
     Aligns inconsistent registration, ip and card country codes to have mostly common values; with a random chance of inconsistencies.
-
+    
     Parameters
     ----------
     series : pandas.Series
         A series from the random transaction dataframe with inconsistent country codes to align.
     proba_comm_ip : float
-        The probability of a common / shared registration country code and ip country code.
+        The probability of a common / shared registration country code and ip country code, default is 0.05.
     proba_comm_card : float
-        The probability of a common / shared registration country code and card country code.
-
+        The probability of a common / shared registration country code and card country code, default is 0.01.
+    
     Returns
     -------
     pandas.Series
@@ -33,16 +33,16 @@ def align_country_codes(
     ip_country_code = series["ip_country_code_alpha"]
     card_country_code = series["card_country_code_alpha"]
     # determine shared or new ip country code
-    if ip_country_code == ip_country_code:
-        if random_unif >= proba_comm_ip:
+    if pd.notna(ip_country_code):
+        if random_unif <= proba_comm_ip:
             new_ip_country_code = ip_country_code
         else:
             new_ip_country_code = registration_country_code
     else:
         new_ip_country_code = np.nan
     # determine shared or new card country code
-    if card_country_code == card_country_code:
-        if random_unif >= proba_comm_card:
+    if pd.notna(card_country_code):
+        if random_unif <= proba_comm_card:
             new_card_country_code = card_country_code
         else:
             new_card_country_code = registration_country_code
