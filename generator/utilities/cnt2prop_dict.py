a8de17c54838cc5e25194345c59d24b03775cdbf
Oisin <oisin.leonard@gmail.com>
2026-01-06 09:43:03 +0000

    Revised cnt 2 prop function with copilot revisions

--- generator/utilities/cnt2prop_dict.py
+++ generator/utilities/cnt2prop_dict.py
@@ -1,27 +1,35 @@
 from beartype import beartype
+from typing import Dict
 
 @beartype
 def cnt2prop_dict(
-    idhash_cnt_dict:dict
-    ) -> dict:
+    idhash_cnt_dict:Dict[str, int],
+    ) -> Dict[str, float]:
     """
     Converts a dictionary of counts to a dictionary of proportions.
-
+    
     Parameters
     ----------
     idhash_cnt_dict : dict
         A dictionary of key, value pairs where the value indicates a count.
-
+    
     Returns
     -------
     dict
         A dictionary of key, value pairs where the value indicates a proportion.
+    
+    Examples
+    --------
+    ```
+    idhash_cnt_dict = {'7125135c8882b0f6': 2, '049dd291d9506532': 3, 'd6708d344cb6f498': 5}
+    prop_dict = cnt2prop_dict(idhash_cnt_dict=idhash_cnt_dict)
+    ```
     """
     # empty dictionary for proportions
     prop_dict = {}
-    # sum of dictionary counts
-    cnt_total = sum(idhash_cnt_dict.values())
-    # iterate over input dictionary and convert counts to proportions
-    for idhash, cnt in idhash_cnt_dict.items():
-        prop_dict[idhash] = cnt / cnt_total
+    if idhash_cnt_dict != {}:
+        # sum of dictionary counts
+        cnt_total = sum(idhash_cnt_dict.values())
+        # iterate over input dictionary and convert counts to proportions
+        prop_dict = {idhash: cnt / cnt_total for idhash, cnt in idhash_cnt_dict.items()}
     return prop_dict
