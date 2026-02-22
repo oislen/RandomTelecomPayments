07348de8d534e64c6549114ae3e72a81db553a38
Oisin <oisin.leonard@gmail.com>
2026-02-22 14:06:57 +0000

    Added is_release parameter logic to drop itr_hash

--- generator/cons.py
+++ generator/cons.py
@@ -56,6 +56,7 @@ default_registration_start_date = (date_today - datetime.timedelta(days=731)).st
 default_registration_end_date = (date_today - datetime.timedelta(days=366)).strftime(date_date_strftime)
 default_transaction_start_date = (date_today - datetime.timedelta(days=365)).strftime(date_date_strftime)
 default_transaction_end_date = date_today.strftime(date_date_strftime)
+default_is_release = False
 # define default input parameters dictionary
 default_input_params_dict = {
     "n_users": default_n_users,
