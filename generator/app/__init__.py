72beb38d67e5a7134b7549761226079c24ebf9b7
Oisin <oisin.leonard@gmail.com>
2026-09-15 20:47:22 +0100

    style(#74): formatted __all__ imports

--- generator/app/__init__.py
+++ generator/app/__init__.py
@@ -3,4 +3,9 @@ from .gen_trans_data import gen_trans_data
 from .gen_user_data import gen_user_data
 from .ProgrammeParams import ProgrammeParams
 
-__all__ = ["gen_random_telecom_data", "gen_trans_data", "gen_user_data", "ProgrammeParams"]
+__all__ = [
+    "gen_random_telecom_data",
+    "gen_trans_data",
+    "gen_user_data",
+    "ProgrammeParams",
+]
